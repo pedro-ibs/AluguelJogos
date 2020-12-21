41326d382a8a3333edf0b0ce3650a4d3de4c619b
pedro-ibs <pedro.igor.ifsp@gmail.com>
2020-12-21 17:06:34 -0300

    criando model Usuário

--- application/controllers/Exemplo.php
+++ application/controllers/Exemplo.php
@@ -7,21 +7,26 @@ class Exemplo extends CI_Controller {
         parent:: __construct();
         $this->data = array();
 
-        $this->load->model("Exemplo_model", "m_exemplo");
+        $this->load->model("Usuario", "m_exemplo");
 
         //Carrega todos os dados da construção da pagina automaticamente.
         //Qualquer pagina que você criar dentro desse controller, vai ter essas paginas carregadas automaticamente.
-        $this->data["header"] = $this->load->view("template/header", $this->data, true);
-        $this->data["navbar"] = $this->load->view("template/navbar", $this->data, true);
-        $this->data["sidebar"] = $this->load->view("template/sidebar", $this->data, true);
-        $this->data["footer"] = $this->load->view("template/footer", $this->data, true);
+        $this->data["header"]   = $this->load->view("template/header",  $this->data, true);
+        $this->data["navbar"]   = $this->load->view("template/navbar",  $this->data, true);
+        $this->data["sidebar"]  = $this->load->view("template/sidebar", $this->data, true);
+        $this->data["footer"]   = $this->load->view("template/footer",  $this->data, true);
     }
 
     public function index()
 	{
-        $this->data["info"] = $this->m_exemplo->get_info();
+        $this->data["info"] = $this->m_exemplo->mostrarUsuario2('affg@gmail.com', '12345');
+        $this->m_exemplo->adicionarUsuario('opa', 'opa', 'opa', 'opa');
+        
+        
         //Utilize sempre esse nome no $this->data["content"].
+
         $this->data["content"] = $this->load->view("home/home", $this->data, true);
-		$this->load->view("template/content", $this->data);
+        
+        $this->load->view("template/content", $this->data);
 	}
 }
