ed279aa5be2fc1489c938cb4ae47954c946bf69e
pedro-ibs <pedro.igor.ifsp@gmail.com>
2020-12-22 15:03:24 -0300

    add model Marca

--- application/models/Usuario.php
+++ application/models/Usuario.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Usuario extends CI_Model{
-    
+
     function __construct() {
         parent::__construct();       
         $this->db = $this->load->database("default", true);
